f7254c013dc79c16e258b2c1f856d498071a4276
Megagun <megagun@mooses.nl>
2011-02-18 20:23:31 +0100

    Now using an INI file for the settings (instead of an XML file) through the ini4j library. Added README.TXT file to list the libraries require & their version numbers Modified LICENSE.TXT file to ad ini4j
    Added README.TXT

--- src/uqm/uqmatchbot/Main.java
+++ src/uqm/uqmatchbot/Main.java
@@ -15,15 +15,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        String host = Settings.getInstance().getSettingString("IRCServer", "chat.freenode.net");
-        String userName = Settings.getInstance().getSettingString("botUsername", "UQMatchbot");
-        String realName = Settings.getInstance().getSettingString("botRealname", "UQMatchbot");
-        String nickName = Settings.getInstance().getSettingString("botNickname", "UQMatchbot");
-        Integer portMin = Settings.getInstance().getSettingInteger("IRCServerPortMin", 6667);
-        Integer portMax = Settings.getInstance().getSettingInteger("IRCServerPortMax", 6667);
-        String botIdent = Settings.getInstance().getSettingString("botIdentityPassword");
-        String htmlLog = Settings.getInstance().getSettingString("botHTMLLogFile");
-        String channel = Settings.getInstance().getSettingString("botChannel");
+        String host = Settings.getInstance().getSetting("IRCServer", "chat.freenode.net");
+        String userName = Settings.getInstance().getSetting("botUsername", "UQMatchbot");
+        String realName = Settings.getInstance().getSetting("botRealname", "UQMatchbot");
+        String nickName = Settings.getInstance().getSetting("botNickname", "UQMatchbot");
+        Integer portMin = Integer.valueOf(Settings.getInstance().getSetting("IRCServerPortMin", "6667"));
+        Integer portMax = Integer.valueOf(Settings.getInstance().getSetting("IRCServerPortMax", "6667"));
+        String botIdent = Settings.getInstance().getSetting("botIdentityPassword");
+        String htmlLog = Settings.getInstance().getSetting("botHTMLLogFile", null);
+        String channel = Settings.getInstance().getSetting("botChannel", "#uqm-arena");
 
         ArrayList<String> channels = new ArrayList<String>();
         if (!channel.startsWith("#")) {
--- src/uqm/uqmatchbot/filestuff/Settings.java
+++ src/uqm/uqmatchbot/filestuff/Settings.java
@@ -4,103 +4,57 @@
  */
 package uqm.uqmatchbot.filestuff;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.thoughtworks.xstream.converters.collections.MapConverter;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-import com.thoughtworks.xstream.mapper.Mapper;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
 
 /**
- * Settings class. Singleton object that manages settings. Saves and loads from an XML file
- * TODO: Move from XStream for saving this to some other library.
+ * Settings class. Singleton object that manages settings.
+ * Saves and loads settings from an .ini file.
  */
 public class Settings {
-    private static final String CONFFILE = "uqmatchbot_conf.xml"; // The configuration filename
+
+    private static final String CONFFILE = "uqmatchbot_conf.ini"; // The configuration filename
     private static Settings instance; // Instance of this object.
-    private static XStream xstream; // Instance of the XStream class for serialization purposes
-    @XStreamAlias("Strings")
     private HashMap<String, String> settings_Strings;
-    @XStreamAlias("Booleans")
-    private HashMap<String, Boolean> settings_Booleans;
-    @XStreamAlias("Integers")
-    private HashMap<String, Integer> settings_Integers;
-    @XStreamAlias("Doubles")
-    private HashMap<String, Double> settings_Doubles;
 
     public static Settings getInstance() {
-        if (xstream == null) {
-            xstream = new XStream(new DomDriver());
-            xstream.alias("Settings", Settings.class);
-            xstream.processAnnotations(Settings.class);
-        }
         if (instance == null) {
+            instance = new Settings();
             File f = new File(CONFFILE);
             if (f.exists()) {
                 try {
-                    instance = (Settings) xstream.fromXML(new FileInputStream(f));
-                } catch (FileNotFoundException ex) {
+                    Wini ini = new Wini(f);
+                    instance.settings_Strings = new HashMap<String, String>(ini.get("Settings"));
+                } catch (InvalidFileFormatException ex) {
+                    Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
                     Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
                 }
             } else {
-                Logger.getLogger(Settings.class.getName()).log(Level.WARNING, "Settings file ''{0}'' does not exist! Not loading settings. Prepare for unforseen consequences.", f.getAbsolutePath());
-                instance = new Settings();
+                Logger.getLogger(Settings.class.getName()).log(Level.WARNING, "Configuration .ini file ''{0}'' not found! Prepare for unforseen consequences. Maybe.", f.getAbsolutePath());
             }
         }
-        instance.init_maps_if_null();
         return instance;
     }
 
     private Settings() {
         settings_Strings = new HashMap<String, String>();
-        settings_Booleans = new HashMap<String, Boolean>();
-        settings_Integers = new HashMap<String, Integer>();
-        settings_Doubles = new HashMap<String, Double>();
-    }
-
-    private void init_maps_if_null() {
-        if (settings_Strings == null) {
-            settings_Strings = new HashMap<String, String>();
-        }
-        if (settings_Booleans == null) {
-            settings_Booleans = new HashMap<String, Boolean>();
-        }
-        if (settings_Integers == null) {
-            settings_Integers = new HashMap<String, Integer>();
-        }
-        if (settings_Doubles == null) {
-            settings_Doubles = new HashMap<String, Double>();
-        }
     }
 
     public void setSetting(String settingName, String settingValue) {
         settings_Strings.put(settingName, settingValue);
     }
 
-    public void setSetting(String settingName, Boolean settingValue) {
-        settings_Booleans.put(settingName, settingValue);
-    }
-
-    public void setSetting(String settingName, Integer settingValue) {
-        settings_Integers.put(settingName, settingValue);
-    }
-
-    public void setSetting(String settingName, Double settingValue) {
-        settings_Doubles.put(settingName, settingValue);
-    }
-
-    public String getSettingString(String settingName) {
+    public String getSetting(String settingName) {
         return settings_Strings.get(settingName);
     }
 
-    public String getSettingString(String settingName, String default_if_null) {
+    public String getSetting(String settingName, String default_if_null) {
         if (!settings_Strings.containsKey(settingName)) {
             return default_if_null;
         } else {
@@ -108,47 +62,11 @@ public class Settings {
         }
     }
 
-    public Integer getSettingInteger(String settingName) {
-        return settings_Integers.get(settingName);
-    }
-
-    public Integer getSettingInteger(String settingName, Integer default_if_null) {
-        if (settings_Integers.containsKey(settingName)) {
-            return settings_Integers.get(settingName);
-        } else {
-            return default_if_null;
-        }
-    }
-
-    public Double getSettingDouble(String settingName) {
-        return settings_Doubles.get(settingName);
-    }
-
-    public Double getSettingDouble(String settingName, Double default_if_null) {
-        if (settings_Doubles.containsKey(settingName)) {
-            return settings_Doubles.get(settingName);
-        } else {
-            return default_if_null;
-        }
-    }
-
-    public Boolean getSettingBoolean(String settingName) {
-        return settings_Booleans.get(settingName);
-    }
-
-    public Boolean getSettingBoolean(String settingName, Boolean default_if_null) {
-        if (settings_Booleans.containsKey(settingName)) {
-            return settings_Booleans.get(settingName);
-        } else {
-            return default_if_null;
-        }
-    }
-
     public void saveSettings() throws IOException {
-        FileWriter fo = null;
-        String xml = xstream.toXML(this);
-        fo = new FileWriter(Settings.CONFFILE);
-        fo.write(xml);
-        fo.close();
+        Wini ini = new Wini();
+        for (String s : settings_Strings.keySet()) {
+            ini.put("Settings", s, settings_Strings.get(s));
+        }
+        ini.store(new File(CONFFILE));
     }
 }
